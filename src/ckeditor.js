b106f89b245bb82f3979fa3c46b2ddd86a04ef49
ShandelleLeigh <ShandelleLovato@gmail.com>
2020-01-02 11:14:55 -0700

    Force pasting as plain text

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -42,5 +42,6 @@ ClassicEditor.defaultConfig = {
 		]
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: 'en'
+	language: 'en',
+	forcePasteAsPlainText: true,
 };
